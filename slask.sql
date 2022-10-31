6587e96d28552aa575e73e8ac79a930f3c1478c1
Joel Blomberg <joel.blomberg@iths.se>
2022-10-31 15:00:32 +0100

    ...

--- slask.sql
+++ slask.sql
@@ -1,7 +1,32 @@
-select
-    'Hej ' + FirstName + '!' as Greeting,
-    len(FirstName + LastName),
-    len(FirstName + ' ' + LastName),
-    len(FirstName) + len(LastName)
-from Users
+--select
+--    'Hej ' + FirstName + '!' as Greeting,
+--    len(FirstName + LastName),
+--    len(FirstName + ' ' + LastName),
+--    len(FirstName) + len(LastName)
+--from Users
 
+
+
+
+SELECT *
+FROM Elements
+WHERE Meltingpoint BETWEEN 100 and 200
+
+SELECT *
+FROM Elements
+WHERE [Period] = 1
+OR Radius > 200
+
+SELECT *
+FROM Elements
+WHERE Radius > Boilingpoint
+
+SELECT *
+FROM Elements
+WHERE Valenceel % 2 = 0
+
+
+
+SELECT *
+FROM Elements
+WHERE Boilingpoint IS NULL AND Meltingpoint IS NULL
